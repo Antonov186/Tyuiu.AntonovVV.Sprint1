bec14befdae5715944f2022ccfe2b27290110a4d
Antonov186 <vasya.hvostic@gmail.com>
2025-06-07 13:05:41 +0500

    Task5

---
+++ Tyuiu.AntonovVV.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AntonovVV.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.AntonovVV.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 3, x2 = 6, y1 = 2, y2 = 6;
+            int res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            int wait = 5;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
