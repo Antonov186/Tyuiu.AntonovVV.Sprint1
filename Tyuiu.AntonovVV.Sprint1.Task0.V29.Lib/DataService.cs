3ae3bbe5e81bc14abf7ff82aa454b7de15b7efc2
Antonov186 <vasya.hvostic@gmail.com>
2025-06-07 12:27:20 +0500

    Task0

--- Tyuiu.AntonovVV.Sprint1.Task0.V29.Lib/DataService.cs
+++ Tyuiu.AntonovVV.Sprint1.Task0.V29.Lib/DataService.cs
@@ -1,9 +1,12 @@
-﻿namespace Tyuiu.AntonovVV.Sprint1.Task0.V29.Lib
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.AntonovVV.Sprint1.Task0.V29.Lib
 {
-    public class DataService
+    public class DataService : ISprint1Task0V29
     {
-
+        public double Calculate()
+        {
+            return 2 * 7 + 9 * 2;
+        }
     }
+
 }
-
-
--- Tyuiu.AntonovVV.Sprint1.Task0.V29.Test/DataServiceTest.cs
+++ Tyuiu.AntonovVV.Sprint1.Task0.V29.Test/DataServiceTest.cs
@@ -5,10 +5,11 @@ namespace Tyuiu.AntonovVV.Sprint1.Task0.V29.Test
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidExpression()
         {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(32, res);
         }
     }
-
 }
-
