eda30d8ea03945e86d733dc2710ed6bde9d5fee9
Antonov186 <vasya.hvostic@gmail.com>
2025-06-07 12:56:47 +0500

    Task3

---
+++ Tyuiu.AntonovVV.Sprint1.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AntonovVV.Sprint1.Task3.V7.Lib;
+namespace Tyuiu.AntonovVV.Sprint1.Task3.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double verst = 100;
+            var res = ds.VerstsToKilometers(verst);
+            Assert.AreEqual(106.68, res);
+        }
+    }
+}
---
+++ Tyuiu.AntonovVV.Sprint1.Task3.V7/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.AntonovVV.Sprint1.Task3.V7.Lib;
+namespace Tyuiu.AntonovVV.Sprint1.Task3.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                *");
+            Console.WriteLine("***************************************************************************************************");
+
+            Console.WriteLine("Введите расстояние в верстах и нажмите <Enter>");
+
+
+
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                      *");
+            Console.WriteLine("***************************************************************************************************");
+
+
+
+
+            double verst = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Расстояние в вёрстах равен: " + ds.VerstsToKilometers(verst));
+            Console.ReadLine();
+
+        }
+    }
+}
