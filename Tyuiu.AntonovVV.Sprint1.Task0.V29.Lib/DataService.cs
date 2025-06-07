b14a15a0487931adf37fcd36f96aeadf883cd93f
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-07 12:16:20 +0500

    Task0

---
+++ Tyuiu.AntonovVV.Sprint1.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.AntonovVV.Sprint1.Task0.V29.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.AntonovVV.Sprint1.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.AntonovVV.Sprint1.Task0.V29.Lib;
+namespace Tyuiu.AntonovVV.Sprint1.Task0.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
