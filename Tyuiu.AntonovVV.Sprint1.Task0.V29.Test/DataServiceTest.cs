7934321c6eadf5fd9a9a4e104f2b80f33aab4d4c
Antonov186 <vasya.hvostic@gmail.com>
2025-06-07 12:47:14 +0500

    Task1

--- Tyuiu.AntonovVV.Sprint1.Task0.V29.Test/DataServiceTest.cs
+++ Tyuiu.AntonovVV.Sprint1.Task0.V29.Test/DataServiceTest.cs
@@ -2,7 +2,7 @@
 namespace Tyuiu.AntonovVV.Sprint1.Task0.V29.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
         public void ValidExpression()
